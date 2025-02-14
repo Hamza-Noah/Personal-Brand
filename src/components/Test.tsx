28eb1582d7dc8b85a0ea5dfc3a41b2a0850a8e21
Hamza-Noah <hamza.nouh97@gmail.com>
2025-02-14 16:59:22 +0400

    removed a test file

--- src/components/Test.tsx
+++
@@ -1,8 +0,0 @@
-import { useTranslationContext } from "../contexts/TranslationContext";
-
-const Test = () => {
-  const { t } = useTranslationContext();
-  return <h2>{t("home.welcome")}</h2>;
-};
-
-export default Test;
