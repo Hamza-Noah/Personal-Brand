82474b6cfe7b720b06da6a6c58fe748fb1fb1c32
Hamza-Noah <hamza.nouh97@gmail.com>
2025-02-22 14:26:07 +0400

    configured the font when changing language

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import { useEffect } from "react";
 import { useTranslationContext } from "./contexts/TranslationContext";
 import i18n from "./i18n";
 import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
 
 function App() {
   const { currentLang } = useTranslationContext();
 
   useEffect(() => {
     document.dir = i18n.dir();
+    updateFontFamily();
   }, [currentLang]);
+
+  const updateFontFamily = () => {
+    if (currentLang === "ar") {
+      document.body.style.fontFamily = "ArabicPoetryExtendedMedium, adoody";
+      document.body.style.fontSize = "1.25rem";
+    } else {
+      document.body.style.fontFamily = "Space Mono, sans-serif";
+      document.body.style.fontSize = ".8rem";
+    }
+  };
 
   return (
     <>
       <Navbar />
+      <Landing />
     </>
   );
 }
