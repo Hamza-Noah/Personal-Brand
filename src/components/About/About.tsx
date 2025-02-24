42181c489591971273720eed466a4ea56e3a6cee
Hamza-Noah <hamza.nouh97@gmail.com>
2025-02-24 23:14:34 +0400

    initiated the about component

---
+++ src/components/About/About.tsx
@@ -0,0 +1,21 @@
+import styles from "./about.module.scss";
+import about1 from "../../assets/image/about1.png";
+import about2 from "../../assets/image/about2.png";
+import about3 from "../../assets/image/about3.png";
+import about4 from "../../assets/image/about4.png";
+import about5 from "../../assets/image/about5.png";
+import aboutBtn1 from "../../assets/svg/about button1.svg";
+import aboutBtn2 from "../../assets/svg/about button2.svg";
+import aboutBtn3 from "../../assets/svg/about button3.svg";
+
+const About = () => {
+  return (
+    <>
+      <div className={`about`}>
+        
+      </div>
+    </>
+  );
+};
+
+export default About;
