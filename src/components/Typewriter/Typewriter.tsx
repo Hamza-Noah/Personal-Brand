8dd6a1bf052eb976e77602be46ea258a5bcd9e4f
Hamza-Noah <hamza.nouh97@gmail.com>
2025-02-24 14:07:10 +0400

    added a fix to avoid a warning

--- src/components/Typewriter/Typewriter.tsx
+++ src/components/Typewriter/Typewriter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import typewriter from "../../assets/image/typewriter.svg";
 
 const Typewriter = ({ text = "test", delay = 500 }) => {
