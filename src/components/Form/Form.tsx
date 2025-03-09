763cf8ef3f84e3ae59384926be16615d6691a785
Hamza-Noah <hamza.nouh97@gmail.com>
2025-03-09 10:48:00 +0400

    finished the UI for form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,18 +1,61 @@
 import styles from "./form.module.scss";
-import form from "../../assets/image/form.svg";
+import { useTranslationContext } from "../../contexts/TranslationContext";
 
 const Form = () => {
+  const { t, currentLang } = useTranslationContext();
+
+  console.log(t("form.emailPlaceholder"));
+
   return (
     <>
-      <form action="">
+      <form className={` ${currentLang == "en" ? "" : "rtl"}`}>
         <div className={`${styles.header} d-flex justify-content-between`}>
           <h3>
-            Wanna Connect
+            {t("form.title")}
             <span className="ps-3">&gt;/</span>
           </h3>
           <div className="buttons">
             <button type="button" className={styles.minimize}></button>
             <button type="button" className={styles.close}></button>
+          </div>
+        </div>
+        <div className={`d-flex flex-wrap ${styles.inputs}`}>
+          <div className={`form-group ${styles.input}`}>
+            <input
+              className="w-100"
+              placeholder={t("form.namePlaceholder")}
+              type="text"
+            />
+          </div>
+          <div className={`form-group ${styles.input}`}>
+            <input
+              className="w-100"
+              placeholder={t("form.emailPlaceholder")}
+              type="email"
+            />
+          </div>
+          <div className="form-group w-100">
+            <textarea
+              name=""
+              placeholder={t("form.emailPlaceholder")}
+              className="w-100"
+            ></textarea>
+          </div>
+          <div
+            className={`form-group ${
+              currentLang == "en" ? "me-auto" : "ms-auto"
+            }`}
+          >
+            <button className={`${styles.submit}`}>
+              TAKE MY{" "}
+              <span
+                className={`form-group ${
+                  currentLang == "en" ? "me-2" : "ms-2"
+                }`}
+              >
+                $
+              </span>
+            </button>
           </div>
         </div>
       </form>
