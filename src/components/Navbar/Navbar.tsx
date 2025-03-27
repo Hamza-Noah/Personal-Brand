c87f34bc69149f68c4965c06fd0358b3f6845608
Hamza-Noah <hamza.nouh97@gmail.com>
2025-03-28 02:57:17 +0400

    replayed the dropdown menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,22 @@
+import { useState } from "react";
 import { useTranslationContext } from "../../contexts/TranslationContext";
-import { useNavbarImage } from "../../hooks/useNavbarImage"; // Import the custom hook
+import { useNavbarImage } from "../../hooks/useNavbarImage";
 import styles from "./navbar.module.scss";
 import mainLogo from "../../../public/signature.svg";
 import menu from "../../assets/svg/menu.svg";
 
 const Navbar = () => {
   const { changeLanguage, currentLang } = useTranslationContext();
-
-  // Get the navbar image from the custom hook
   const navbarImage = useNavbarImage();
 
+  // State to toggle dropdown visibility
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
-    <div className={`${styles.navbar}`}>
+    <div className={`${styles.navbar} ${currentLang === "en" ? styles.en : styles.ar}`}>
       <div className="container-fluid">
         <div className="d-flex justify-content-between align-items-center">
-          <div className="language-buttons d-none d-lg-block fw-bolder">
+          <div className={`${styles["language-buttons"]} d-none d-lg-block fw-bolder`}>
             <button
               className={`${styles["language-btn"]} ${
                 currentLang === "ar" ? styles["language-btn-active"] : ""
@@ -32,44 +34,68 @@ const Navbar = () => {
               EN
             </button>
           </div>
-          <div className="dropdown d-lg-none position-static">
+
+          {/* Mobile Dropdown */}
+          <div className="dropdown d-lg-none position-relative w-100">
             <button
-              className={`btn border-0 dropdown-toggle ${styles["dropdown-toggle"]}`}
+              className={`btn border-0 ${styles["dropdown-toggle"]} w-100 text-start`}
               type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
             >
-              <img src={menu} alt="" />
+              <img src={menu} alt="Menu" />
             </button>
-            <ul className={`dropdown-menu ${styles["dropdown-menu"]}`}>
-              <li>
-                {" "}
-                <button
-                  className={`${styles["language-btn"]} ${
-                    currentLang === "ar" ? styles["language-btn-active"] : ""
-                  }`}
-                  onClick={() => changeLanguage("ar")}
-                >
-                  AR
-                </button>
-              </li>
-              <li>
-                <button
-                  className={`${styles["language-btn"]} ${
-                    currentLang === "en" ? styles["language-btn-active"] : ""
-                  }`}
-                  onClick={() => changeLanguage("en")}
-                >
-                  EN
-                </button>
-              </li>
-            </ul>
+
+            {isDropdownOpen && (
+              <ul
+                className={`dropdown-menu show ${styles["dropdown-menu"]}`}
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  top: "100%",
+                  width: "120%",
+                  backgroundColor: "#140f13",
+                  border: "none",
+                  borderRadius: 0,
+                  margin: "0 -12px",
+                  paddingLeft: 20
+                }}
+              >
+                <li>
+                  <button
+                    className={`${styles["language-btn"]} ${
+                      currentLang === "ar" ? styles["language-btn-active"] : ""
+                    } w-100 text-start`}
+                    onClick={() => {
+                      changeLanguage("ar");
+                      setIsDropdownOpen(false);
+                    }}
+                  >
+                    AR
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className={`${styles["language-btn"]} ${
+                      currentLang === "en" ? styles["language-btn-active"] : ""
+                    } w-100 text-start`}
+                    onClick={() => {
+                      changeLanguage("en");
+                      setIsDropdownOpen(false);
+                    }}
+                  >
+                    EN
+                  </button>
+                </li>
+              </ul>
+            )}
           </div>
-          <div className={`${styles["logo"]}`}>
+
+          {/* Logos */}
+          <div className={styles["logo"]}>
             <img height="24px" src={navbarImage} alt="Navbar Logo" />
           </div>
           <div className="logo">
-            <img height="24px" src={mainLogo} alt="Main Logo" />
+            <img height="28" src={mainLogo} alt="Main Logo" />
           </div>
         </div>
       </div>
